43bd7ddb16a0da3678a48948ef6d2f3c62c97539
mqiu <qiujingming@hotmail.com>
2017-08-09 16:37:02 -0700

    modify the script to test test-sts-mqtt server

--- benchmarks/bombing.js
+++ benchmarks/bombing.js
@@ -1,10 +1,24 @@
 #! /usr/bin/env node
 
 var mqtt = require('../')
-var client = mqtt.connect({ port: 1883, host: 'localhost', clean: true, keepalive: 0 })
+
+console.log('testrobot2-'+process.argv[2]);
+
+var client = mqtt.connect({ 
+  port: 1883, 
+  host: 'test-mqtt.sensetecnic.com', 
+  clean: true, 
+  keepalive: 0,
+  username: 'testrobot2',
+  clientId: 'testrobot2-'+process.argv[2], //testrobot2-00001 
+  password: '12345678' //12345678
+ })
 
 var sent = 0
 var interval = 5000
+var delayMillis = 0; //1 second
+
+
 
 function count () {
   console.log('sent/s', sent / interval * 1000)
@@ -14,8 +28,10 @@ function count () {
 setInterval(count, interval)
 
 function publish () {
-  sent++
-  client.publish('test', 'payload', publish)
+  setTimeout(function() {
+    sent++
+    client.publish('users/testrobot2/loadtest', 'payload', publish)
+  }, delayMillis);
 }
 
 client.on('connect', publish)
--- benchmarks/throughputCounter.js
+++ benchmarks/throughputCounter.js
@@ -2,12 +2,36 @@
 
 var mqtt = require('../')
 
-var client = mqtt.connect({ port: 1883, host: 'localhost', clean: true, encoding: 'binary', keepalive: 0 })
+var client = mqtt.connect({ 
+  port: 1883, 
+  host: 'test-mqtt.sensetecnic.com', 
+  clean: true, 
+  encoding: 'binary', 
+  keepalive: 0,
+  username: 'testrobot2',
+  clientId: 'testrobot2-00001', 
+  password: '12345678' //12345678
+})
 var counter = 0
+var max = 0
 var interval = 5000
+var getAvg = []
+
+var averageRange = 10
 
 function count () {
-  console.log('received/s', counter / interval * 1000)
+  var sum = 0;
+  var avg = 0
+  var countDuringInterval = counter / interval * 1000
+  getAvg.unshift(countDuringInterval);
+  if(getAvg.length > averageRange) {getAvg.pop()}
+  for(i=0;i<getAvg.length;i++) {
+    sum += getAvg[i]
+  }
+  avg = sum / getAvg.length;
+
+  if (countDuringInterval > max) { max = countDuringInterval}
+  console.log('max: ' + max.toFixed(2) + ', average: ' + avg.toFixed(2) + ', received/s: ' + countDuringInterval.toFixed(2) )
   counter = 0
 }
 
@@ -15,8 +39,12 @@ setInterval(count, interval)
 
 client.on('connect', function () {
   count()
-  this.subscribe('test')
+  this.subscribe('users/testrobot2/loadtest')
   this.on('message', function () {
     counter++
   })
+})
+
+client.on('error', function () {
+  console.log("error!")
 })
