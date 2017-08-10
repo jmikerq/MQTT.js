d4625fe488ca41ebc48edb853e004303a24afcf6
mqiu <qiujingming@hotmail.com>
2017-08-10 11:46:00 -0700

    automate bombing script

--- benchmarks/bombing.js
+++ benchmarks/bombing.js
@@ -1,42 +1,62 @@
 #! /usr/bin/env node
 
 var mqtt = require('../')
+var keypress = require('keypress')
 
-console.log('testrobot2-'+process.argv[2]);
+keypress(process.stdin)
+var clientCount = 2
 
-var client = mqtt.connect({ 
-  port: 1883, 
-  host: 'test-mqtt.sensetecnic.com', 
-  clean: true, 
-  keepalive: 0,
-  username: 'testrobot2',
-  clientId: 'testrobot2-'+process.argv[2], //testrobot2-00001 
-  password: '12345678' //12345678
- })
+console.log('start')
 
-var sent = 0
-var interval = 5000
-var delayMillis = 0; //1 second
-
-
-
-function count () {
-  console.log('sent/s', sent / interval * 1000)
-  sent = 0
-}
-
-setInterval(count, interval)
-
-function publish () {
-  setTimeout(function() {
-    sent++
-    client.publish('users/testrobot2/loadtest', 'payload', publish)
-  }, delayMillis);
-}
-
-client.on('connect', publish)
-
-client.on('error', function () {
-  console.log('reconnect!')
-  client.stream.end()
+process.stdin.on('keypress', function (ch, key) {
+  if (key && key.name === 'b') {
+    console.log('Add new bombing client' + clientCount)
+    new bombingClient(clientCount)
+    clientCount++
+  }
+  if (key && key.ctrl && key.name == 'c') {
+    process.exit(1)
+  }
 })
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+function bombingClient(id) {
+  var sent = 0;
+  var interval = 5000;
+  var delayMillis = 0; //1 second
+  // var id = id;
+  var clientId = (id>9)? 'testrobot2-000'+id : 'testrobot2-0000'+id;
+  console.log('Start new client ' + clientId);
+  var client = mqtt.connect({
+    port: 1883, 
+    host: 'test-mqtt.sensetecnic.com', 
+    clean: true, 
+    keepalive: 0,
+    username: 'testrobot2',
+    clientId: clientId, //testrobot2-00001 
+    password: '12345678' //12345678    
+  });
+
+  function count () {
+    console.log(clientId + ": sent/s", sent / interval * 1000);
+    sent = 0;
+  }
+
+  setInterval(count, interval);
+
+  function publish () {
+    setTimeout(function() {
+      sent++;
+      client.publish('users/testrobot2/loadtest', 'payload', publish);
+    }, delayMillis);
+  }
+
+  client.on('connect', publish);
+
+  client.on('error', function () {
+    console.log(clientId +' reconnect!');
+    client.stream.end();
+  })
+}
--- benchmarks/throughputCounter.js
+++ benchmarks/throughputCounter.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-var mqtt = require('../')
+var mqtt = require('../');
 
 var client = mqtt.connect({ 
   port: 1883, 
@@ -11,40 +11,40 @@ var client = mqtt.connect({
   username: 'testrobot2',
   clientId: 'testrobot2-00001', 
   password: '12345678' //12345678
-})
-var counter = 0
-var max = 0
-var interval = 5000
-var getAvg = []
+});
+var counter = 0;
+var max = 0;
+var interval = 5000;
+var getAvg = [];
 
-var averageRange = 10
+var averageRange = 10;
 
 function count () {
   var sum = 0;
-  var avg = 0
-  var countDuringInterval = counter / interval * 1000
+  var avg = 0;
+  var countDuringInterval = counter / interval * 1000;
   getAvg.unshift(countDuringInterval);
-  if(getAvg.length > averageRange) {getAvg.pop()}
-  for(i=0;i<getAvg.length;i++) {
+  if(getAvg.length > averageRange) {getAvg.pop()};
+  for(var i=0;i<getAvg.length;i++) {
     sum += getAvg[i]
-  }
+  };
   avg = sum / getAvg.length;
 
   if (countDuringInterval > max) { max = countDuringInterval}
-  console.log('max: ' + max.toFixed(2) + ', average: ' + avg.toFixed(2) + ', received/s: ' + countDuringInterval.toFixed(2) )
-  counter = 0
+  console.log('max: ' + max.toFixed(2) + ', average: ' + avg.toFixed(2) + ', received/s: ' + countDuringInterval.toFixed(2) );
+  counter = 0;
 }
 
-setInterval(count, interval)
+setInterval(count, interval);
 
 client.on('connect', function () {
-  count()
-  this.subscribe('users/testrobot2/loadtest')
+  count();
+  this.subscribe('users/testrobot2/loadtest');
   this.on('message', function () {
-    counter++
-  })
+    counter++;
+  });
 })
 
 client.on('error', function () {
-  console.log("error!")
+  console.log("error!");
 })
